8641ca431d7b85165f5db1504af4e75e7882f344
SoybalAhmmed <soybalahmed@gmail.com>
2022-05-26 09:45:42 +0600

    best books

--- src/Pages/Home/BestBooks.js
+++ src/Pages/Home/BestBooks.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import doctor from '../../images/doctor.png';
+import best from '../../images/best.png';
+import appointment from '../../images/appointment.png';
 
 const BestBooks = () => {
     return (
@@ -8,13 +9,13 @@ const BestBooks = () => {
         }} 
         className='flex justify-center items-center'>
             <div className='flex-1 hidden lg:block'>
-                <img className='mt-[-100px]' src={doctor} alt="" />
+                <img className='mt-[-100px]' src={best} alt="" />
             </div>
             <div className='flex-1'>
                 <h3 className='text-xl text-primary font-bold'>Appointment</h3>
                 <h2 className='text-3xl text-white'>Make an Appointment Today</h2>
                 <p className='text-white'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente earum ab cupiditate autem accusantium expedita sequi, temporibus, aut illo doloribus quaerat explicabo, assumenda consectetur est vel ad sed maiores doloremque consequatur. Amet consequuntur quibusdam autem, quod maxime qui itaque quaerat.</p>
-                <PrimaryButton>Get Started</PrimaryButton>
+                
             </div>
         </section>
     );
