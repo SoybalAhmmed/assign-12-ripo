f277b93a3d433ae9c803b3545dc4b23b3a191f8c
SoybalAhmmed <soybalahmed@gmail.com>
2022-05-26 07:29:02 +0600

    navbar added

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        booktheme: {
+          primary: "#eb347d",
+          secondary: "#fa96be",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
