485ae71ca576cc479bea6ed150eefb96b1a934f6
SoybalAhmmed <soybalahmed@gmail.com>
2022-05-26 10:00:41 +0600

    testimonals added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,6 +3,7 @@ import Banner from './Banner';
 import BestBooks from './BestBooks';
 import Info from './Info';
 import OurBooks from './OurBooks';
+import Testimonials from './Testiomanals';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <Info></Info>
             <OurBooks></OurBooks>
             <BestBooks></BestBooks>
+            <Testimonials></Testimonials>
         </div>
     );
 };
