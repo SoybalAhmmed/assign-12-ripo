be3f1fb165d6bb911f1ed6116cca95072fa4c1f1
SoybalAhmmed <soybalahmed@gmail.com>
2022-05-28 05:02:55 +0600

    token added

--- src/Pages/Dashboard/MyOrder.js
+++ src/Pages/Dashboard/MyOrder.js
@@ -18,8 +18,8 @@ const MyOrder = () => {
     return (
         <div>
         <h2>My Orders: {orders.length}</h2>
-        <div class="overflow-x-auto">
-            <table class="table w-full">
+        <div className="overflow-x-auto">
+            <table className="table w-full">
                 <thead>
                     <tr>
                         <th></th>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,6 +3,7 @@ import {  useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-fireb
 import { useForm } from "react-hook-form";
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import auth from '../../firebase.init';
+import useToken from '../../hooks/useToken';
 import Loading from '../Shared/Loading';
 
 const Login = () => {
@@ -14,6 +15,7 @@ const Login = () => {
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth);
+    const [token] = useToken(user || gUser);
 
     let signInError;
     const navigate = useNavigate();
@@ -21,10 +23,10 @@ const Login = () => {
     let from = location.state?.from?.pathname || "/";
 
     useEffect( () =>{
-        if (user || gUser) {
+        if (token) {
             navigate(from, { replace: true });
         }
-    }, [user, gUser, from, navigate])
+    }, [token, from, navigate])
 
     if (loading || gLoading) {
         return <Loading></Loading>
